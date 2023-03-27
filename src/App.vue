9affa0b53f1a963805a3d63e79ec2187cc65fca9
PaoloWD <paolo.lore.93@gmail.com>
2023-03-27 12:51:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Container_img></Container_img>
+  </div>
+</template>
+
+<script>
+import Container_img from "./components/Container_img.vue";
+import { store } from "./store/store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { Container_img },
+};
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/Container_img.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+import { store } from "../store/store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
