a16099441341c20d3c4ab0180879ceba813f4132
PaoloWD <paolo.lore.93@gmail.com>
2023-04-01 19:32:29 +0200

    add logo png and create a responsive header

--- src/components/Container_img.vue
+++ src/components/Container_img.vue
@@ -1,25 +1,88 @@
 <template>
-  <div class="jumbotron"></div>
-  <div>
-    <div class="container"></div>
+  <div
+    class="jumbotron custom-bg-dark row justify-content-between align-items-center"
+  >
+    <div
+      class="col-sm-6 logo-small py-3 text-center"
+      v-if="screenSize === 'sm'"
+    >
+      <img src="../../public/logo small.png" alt="" />
+    </div>
+    <div class="col-lg-6 p-5 logo-large" v-else>
+      <img src="../../public/LOGO ORIZZONTALE PL SMALL.png" alt="" />
+    </div>
+    <div class="col-sm-6" v-if="screenSize === 'sm'"></div>
+    <div class="p-5 col-lg-6" v-else>
+      <div class="d-flex justify-content-end gap-5">
+        <div>
+          <i class="fa-brands fa-instagram"></i>
+        </div>
+        <div>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/paolo-lo-re-07999b244/"
+            ><i class="fa-brands fa-linkedin"></i
+          ></a>
+        </div>
+        <div>
+          <a target="_blank" href="https://github.com/PaoloWD"
+            ><i class="fa-brands fa-github"></i
+          ></a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { store } from "../store/store";
+
 export default {
   data() {
     return {
       store,
+      screenSize: "",
     };
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      if (window.innerWidth < 992) {
+        this.screenSize = "sm";
+      } else {
+        this.screenSize = "lg";
+      }
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
+.logo {
+  text-align: center;
+}
 .jumbotron {
-  background-image: url("https://us.123rf.com/450wm/phochi1981/phochi19812006/phochi1981200600003/149776758-linee-dorate-a-strisce-astratte-su-sfondo-nero-con-spazio-per-il-testo-stile-di-lusso-%C3%A8-possibile.jpg?ver=6");
-  height: 30vh;
-  background-size: cover;
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.logo-small {
+  img {
+    height: 50px;
+  }
+}
+.logo-large {
+  img {
+    height: 90px;
+  }
+}
+a {
+  color: white;
 }
 </style>
